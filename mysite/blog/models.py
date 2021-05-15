0a5fd850774e8d6069144865f91eab56679bc6e1
Oluwafemi <phemmylintry@gmail.com>
2021-05-15 15:42:59 +0100

    django templating

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    
+    author = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, this is my first django app")
+    
+    posts = Post.objects.all()
+
+    context = {
+        "posts" : posts
+    }
+
+    return render(request, "blog/index.html", context)
